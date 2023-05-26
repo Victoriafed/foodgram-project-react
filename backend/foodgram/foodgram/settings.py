f07d65575185d021417dd0061f64336f392ff9d1
Victoriafed <victoriafedorova2018@mail.ru>
2023-05-26 17:24:00 +0300

    v0.6.3 fix front initial

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -148,7 +148,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.AllowAny',
-        'rest_framework.authentication.SessionAuthentication'
     ],
 
     'DEFAULT_AUTHENTICATION_CLASSES': [
