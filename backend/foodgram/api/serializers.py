4c3c0aa39d6dcc42d62f074058e9c51e6ca7050e
Victoriafed <victoriafedorova2018@mail.ru>
2023-05-27 12:00:19 +0300

    v0.7.1  +password

--- backend/foodgram/api/serializers.py
+++ backend/foodgram/api/serializers.py
@@ -22,13 +22,27 @@ class UserSerializer(serializers.ModelSerializer):
             'first_name',
             'last_name',
             'is_subscribed',
+            'password',
         )
+        extra_kwargs = {'password': {'write_only': True}}
+        read_only_fields = 'is_subscribed',
 
     def get_is_subscribed(self, obj):
         user = self.context.get('request').user
         if user.is_anonymous:
             return False
         return Subscribe.objects.filter(user=user, author=obj).exists()
+
+    def create(self, validated_data: dict) -> User:
+        user = User(
+            email=validated_data['email'],
+            username=validated_data['username'],
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name'],
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
 
 
 class UserCreateSerializer(serializers.ModelSerializer):
@@ -41,7 +55,20 @@ class UserCreateSerializer(serializers.ModelSerializer):
             'username',
             'first_name',
             'last_name',
+            'password',
         )
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data: dict) -> User:
+        user = User(
+            email=validated_data['email'],
+            username=validated_data['username'],
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name'],
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
 
 
 class TagSerializer(serializers.ModelSerializer):
