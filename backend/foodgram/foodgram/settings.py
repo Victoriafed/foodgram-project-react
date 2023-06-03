c98d2eca0c3f2877e209058491fffe70299d420b
Victoriafed <victoriafedorova2018@mail.ru>
2023-06-03 23:51:41 +0300

    debug

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2bn(sz+@p4idu&^v^_1m(6bnh^o!@)c$-$(3!ny(o#2q59rqrz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '158.160.50.23',
