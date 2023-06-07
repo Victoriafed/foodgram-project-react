7026431efd1964de7cd80df5c2d0991a6f32aebf
Victoriafed <victoriafedorova2018@mail.ru>
2023-06-07 14:12:44 +0300

    v0.8.5 + recipes_count

--- backend/foodgram/api/serializers.py
+++ backend/foodgram/api/serializers.py
@@ -193,21 +193,6 @@ class SubscribeSerializer(UserSerializer):
         )
         read_only_fields = ('email', 'username')
 
-    def validate(self, data):
-        author = self.instance
-        user = self.context.get('request').user
-        if Subscribe.objects.filter(author=author, user=user).exists():
-            raise ValidationError(
-                detail='Вы уже подписаны на этого пользователя!',
-                code=status.HTTP_400_BAD_REQUEST
-            )
-        if user == author:
-            raise ValidationError(
-                detail='Вы не можете подписаться на самого себя!',
-                code=status.HTTP_400_BAD_REQUEST
-            )
-        return data
-
     def get_recipes_count(self, obj):
         return obj.recipes.count()
 
