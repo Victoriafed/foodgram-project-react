eb7d52ae93023a77f33839f1e110a1bc6d710817
Victoriafed <victoriafedorova2018@mail.ru>
2023-06-07 15:02:39 +0300

    v0.8.5 + recipes_count

--- backend/foodgram/api/views.py
+++ backend/foodgram/api/views.py
@@ -5,7 +5,8 @@ from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
 from recipes.models import (Favorite, Ingredient, Recipe, RecipeIngredient,
-                            ShoppingСart, Tag, Subdcribe)
+                            ShoppingСart, Tag)
+from users.models import Subdcribe
 from rest_framework import permissions, status, viewsets
 from rest_framework.decorators import action
 from rest_framework.permissions import SAFE_METHODS
