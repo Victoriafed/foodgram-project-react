c15d23b95bd30d59aff9c78db447543f45dae329
Victoriafed <victoriafedorova2018@mail.ru>
2023-06-07 14:05:02 +0300

    v0.8.5 + recipes_count

--- backend/foodgram/api/serializers.py
+++ backend/foodgram/api/serializers.py
@@ -183,41 +183,43 @@ class RecipeSubscribeSerializer(serializers.ModelSerializer):
         )
 
 
-class SubscribeSerializer(serializers.ModelSerializer):
-    recipes = RecipeSubscribeSerializer(many=True, read_only=True)
+class SubscribeSerializer(UserSerializer):
     recipes_count = SerializerMethodField()
+    recipes = SerializerMethodField()
 
-    class Meta:
-        model = User
-        fields = (
-            'id',
-            'email',
-            'username',
-            'first_name',
-            'last_name',
-            'is_subscribed',
-            'recipes',
-            'recipes_count'
+    class Meta(UserSerializer.Meta):
+        fields = UserSerializer.Meta.fields + (
+            'recipes_count', 'recipes'
         )
+        read_only_fields = ('email', 'username')
 
-    def get_is_subscribed(self, obj):
-        return Subscribe.objects.filter(
-            user=obj.user,
-            author=obj.author
-        ).exists()
+    def validate(self, data):
+        author = self.instance
+        user = self.context.get('request').user
+        if Subscribe.objects.filter(author=author, user=user).exists():
+            raise ValidationError(
+                detail='Вы уже подписаны на этого пользователя!',
+                code=status.HTTP_400_BAD_REQUEST
+            )
+        if user == author:
+            raise ValidationError(
+                detail='Вы не можете подписаться на самого себя!',
+                code=status.HTTP_400_BAD_REQUEST
+            )
+        return data
+
+    def get_recipes_count(self, obj):
+        return obj.recipes.count()
 
     def get_recipes(self, obj):
         request = self.context.get('request')
         limit = request.GET.get('recipes_limit')
-        queryset = Recipe.objects.filter(author=obj.author)
+        recipes = obj.recipes.all()
         if limit:
-            queryset = queryset[:int(limit)]
-        return RecipeSubscribeSerializer(queryset, many=True).data
-
-    def get_recipes_count(self, obj):
-        queryset = Recipe.objects.filter(author=obj.author.id).count()
-        return queryset
-
+            recipes = recipes[:int(limit)]
+        serializer = RecipeIngredientSerializer(recipes, many=True,
+                                           read_only=True)
+        return serializer.data
 """
     def validate(self, data):
         author = self.instance
