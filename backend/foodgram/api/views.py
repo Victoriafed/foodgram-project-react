4d5c62a87c0d21193ca23269aa5726de5c734b6c
Victoriafed <victoriafedorova2018@mail.ru>
2023-06-11 13:32:57 +0300

    v0.9 setup.cfg

--- backend/foodgram/api/views.py
+++ backend/foodgram/api/views.py
@@ -4,14 +4,13 @@ from django.db.models import Sum
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
+from recipes.models import (Favorite, Ingredient, Recipe, RecipeIngredient,
+                            ShoppingСart, Tag)
 from rest_framework import permissions, status, viewsets
 from rest_framework.decorators import action
 from rest_framework.permissions import SAFE_METHODS
 from rest_framework.response import Response
 from rest_framework.status import HTTP_400_BAD_REQUEST
-
-from recipes.models import (Favorite, Ingredient, Recipe, RecipeIngredient,
-                            ShoppingСart, Tag)
 
 from .filters import IngredientFilter, RecipeFilter
 from .pagination import CustomPagination
