2427b162a95812c1882a850668ea307aba68b97e
Victoriafed <victoriafedorova2018@mail.ru>
2023-06-19 22:56:58 +0300

    v0.2 usercreate

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -144,7 +144,7 @@ DJOSER = {
     'LOGIN_FIELD': 'email',
     'HIDE_USERS': False,
     'SERIALIZERS': {
-        'user_create': 'api.serializers.UsersCreateSerializer',
+        'user_create': 'api.serializers.UserCreateSerializer',
         'user': 'api.serializers.UserSerializer',
         'current_user': 'api.serializers.UserSerializer',
     },
