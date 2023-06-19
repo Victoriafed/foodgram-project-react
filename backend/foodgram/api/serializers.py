6a694fae0b5950c18aabe148af35dfefc13bdadb
Victoriafed <victoriafedorova2018@mail.ru>
2023-06-19 17:27:16 +0300

    v0.2 fields

--- backend/foodgram/api/serializers.py
+++ backend/foodgram/api/serializers.py
@@ -73,6 +73,12 @@ class IngredientSerializer(serializers.ModelSerializer):
 
 
 class IngredientInRecipeSerializer(serializers.ModelSerializer):
+    id = serializers.ReadOnlyField(source='ingredient.id')
+    name = serializers.ReadOnlyField(source='ingredient.name')
+    measurement_unit = serializers.ReadOnlyField(
+        source='ingredient.measurement_unit'
+    )
+
     class Meta:
         model = IngredientInRecipe
         fields = (
