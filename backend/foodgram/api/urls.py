21394723bc75c713592d620fd4e79e1c920abe3a
Victoriafed <victoriafedorova2018@mail.ru>
2023-06-21 11:39:30 +0300

    v0.2 cart

--- backend/foodgram/api/urls.py
+++ backend/foodgram/api/urls.py
@@ -13,6 +13,6 @@ router.register('users', UserViewSet, basename='users')
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('auth/', include('djoser.urls')),
+    path('', include('djoser.urls')),
     path('auth/', include('djoser.urls.authtoken')),
 ]
