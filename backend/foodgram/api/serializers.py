555ce913b761253b46968670a204f87c39f34b04
Victoriafed <victoriafedorova2018@mail.ru>
2023-06-22 20:19:22 +0300

    v0.3 fix keyerror + user in sub

--- backend/foodgram/api/serializers.py
+++ backend/foodgram/api/serializers.py
@@ -208,12 +208,7 @@ class SubscriptionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Subscription
         fields = (
-            'email',
-            'id',
-            'username',
-            'first_name',
-            'last_name',
-            'is_subscribed',
+            'user',
             'recipes',
             'recipes_count'
         )
