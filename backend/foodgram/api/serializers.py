38f9d5e03f3ff01045f34b2f3ce8a1d443326561
Victoriafed <victoriafedorova2018@mail.ru>
2023-06-26 14:49:01 +0300

    v0.3 'recipes',

--- backend/foodgram/api/serializers.py
+++ backend/foodgram/api/serializers.py
@@ -318,7 +318,7 @@ class SubscriptionSerializer(serializers.ModelSerializer):
             'recipes',
             'recipes_count'
         )
-        depth = 1
+
 
     #hhh
     def get_recipes(self, obj):
