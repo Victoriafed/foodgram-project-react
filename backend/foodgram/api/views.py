c93cc7da4b5ade1c311c078670b497e1388e567a
Victoriafed <victoriafedorova2018@mail.ru>
2023-06-27 15:20:35 +0300

    v0.3  list_ingredients

--- backend/foodgram/api/views.py
+++ backend/foodgram/api/views.py
@@ -110,22 +110,11 @@ class RecipeViewSet(viewsets.ModelViewSet):
         permission_classes=[permissions.IsAuthenticated]
     )
     def download_shopping_cart(self, request):
-        # Create a file-like buffer to receive PDF data.
         buffer = io.BytesIO()
-
-        # Create the PDF object, using the buffer as its "file."
-        p = canvas.Canvas(buffer)
-
-        # Draw things on the PDF. Here's where the PDF generation happens.
-        # See the ReportLab documentation for the full list of functionality.
-        p.drawString(100, 100, "Hello world.")
-
-        # Close the PDF object cleanly, and we're done.
-        p.showPage()
-        p.save()
-
-        # FileResponse sets the Content-Disposition header so that browsers
-        # present the option to save the file.
+        c = canvas.Canvas(buffer)
+        c.drawString(100, 100, "Список покупок")
+        c.showPage()
+        c.save()
         buffer.seek(0)
         return FileResponse(buffer, as_attachment=True, filename="hello.pdf")
         '''buffer = io.BytesIO()
