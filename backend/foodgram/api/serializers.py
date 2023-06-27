459805a6f0f22c16957818d98ea12047189104e2
Victoriafed <victoriafedorova2018@mail.ru>
2023-06-27 22:51:34 +0300

    Revert "v0.4 IIR"
    This reverts commit 859450282c6f71d1f75497d1945acdacd3a0b5b4.

--- backend/foodgram/api/serializers.py
+++ backend/foodgram/api/serializers.py
@@ -93,12 +93,7 @@ class IngredientInRecipeSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = IngredientInRecipe
-        fields = (
-            'id',
-            'name',
-            'measurement_unit',
-            'amount'
-        )
+        fields = ('id', 'name', 'measurement_unit', 'amount')
 
 
 class RecipeSerializer(serializers.ModelSerializer):
@@ -218,6 +213,19 @@ class RecipeSerializer(serializers.ModelSerializer):
         if 'tags' in self.validated_data:
             recipe.tags.set(validated_data.pop('tags'))
         return super().update(recipe, validated_data)'''
+
+        # fdhhfdh
+    def to_representation(self, instance):
+        self.fields.pop('ingredients')
+        self.fields.pop('tags')
+        representation = super().to_representation(instance)
+        representation['ingredients'] = IngredientInRecipeSerializer(
+            IngredientInRecipe.objects.filter(recipe=instance), many=True
+        ).data
+        representation['tags'] = TagSerializer(
+            instance.tags, many=True
+        ).data
+        return representation
 
 
 class ShortRecipeSerializer(serializers.ModelSerializer):
