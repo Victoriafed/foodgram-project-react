58f35036435b754c5b494a1f4bd0974291506222
Victoriafed <victoriafedorova2018@mail.ru>
2023-06-28 13:46:03 +0300

    Revert "v0.4 filter"
    This reverts commit dc67d58bd7cbacc747c7775e7313253f570f35be.

--- backend/foodgram/api/filters.py
+++ backend/foodgram/api/filters.py
@@ -3,6 +3,17 @@ from django_filters import rest_framework as filters
 from recipes.models import Recipe, Tag
 
 
+class IngredientFilter(filters.FilterSet):
+    name = filters.CharFilter(
+        field_name='name',
+        lookup_expr='istartswith',
+    )
+
+    class Meta:
+        model = Ingredient
+        fields = ('name', )
+
+
 class RecipeFilter(filters.FilterSet):
     tags = filters.filters.ModelMultipleChoiceFilter(
         queryset=Tag.objects.all(),
