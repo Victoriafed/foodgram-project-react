cc4054b7c4561fdac330a9de5d0dcb13aaa1fb23
Victoriafed <victoriafedorova2018@mail.ru>
2023-06-30 00:06:01 +0300

    v1.0 ingredients

--- backend/foodgram/api/serializers.py
+++ backend/foodgram/api/serializers.py
@@ -108,7 +108,7 @@ class IngredientReadSerializer(serializers.ModelSerializer):
 class RecipeReadSerializer(serializers.ModelSerializer):
     author = UserSerializer(read_only=True)
     tags = TagSerializer(many=True, read_only=True)
-    ingredients = IngredientReadSerializer(many=True, read_only=True)
+    ingredients = IngredientReadSerializer(many=True)
     is_favorited = serializers.SerializerMethodField(read_only=True)
     is_in_shopping_cart = serializers.SerializerMethodField(read_only=True)
     image = Base64ImageField()
