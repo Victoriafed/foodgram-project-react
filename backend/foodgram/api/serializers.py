23e9d1f3e3eee6ef8a338f4006c603737bd94a43
Victoriafed <victoriafedorova2018@mail.ru>
2023-07-01 06:04:04 +0300

    v2.0 SubscriptionSerializer

--- backend/foodgram/api/serializers.py
+++ backend/foodgram/api/serializers.py
@@ -232,10 +232,7 @@ class SubscriptionSerializer(serializers.ModelSerializer):
     username = serializers.ReadOnlyField(source='author.username')
     first_name = serializers.ReadOnlyField(source='author.first_name')
     last_name = serializers.ReadOnlyField(source='author.last_name')
-    recipes = serializers.PrimaryKeyRelatedField(
-        queryset=Recipe.objects.all(),
-        many=True,
-    )
+    recipes = ShortRecipeSerializer(many=True)
     recipes_count = serializers.SerializerMethodField()
     is_subscribed = serializers.SerializerMethodField()
 
