47574e07ff954fcf4248ce4a562ac10e21f20ec9
Victoriafed <victoriafedorova2018@mail.ru>
2023-07-03 21:23:01 +0300

    v2.1 ingredients

--- backend/foodgram/api/serializers.py
+++ backend/foodgram/api/serializers.py
@@ -193,6 +193,7 @@ class RecipeSerializer(serializers.ModelSerializer):
         ingredients = validated_data.pop('ingredients')
         tags = validated_data.pop('tags')
         IngredientInRecipe.objects.filter(recipe=recipe).delete()
+        recipe.ingredients.clear()
         self.add_ingredients(recipe, ingredients)
         recipe.tags.set(tags)
         return super().update(recipe, validated_data)
