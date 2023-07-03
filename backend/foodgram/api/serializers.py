ba7e8a0849865740104faec78d2e99692f15f68d
Victoriafed <victoriafedorova2018@mail.ru>
2023-07-03 21:06:43 +0300

    v2.1 update

--- backend/foodgram/api/serializers.py
+++ backend/foodgram/api/serializers.py
@@ -189,22 +189,13 @@ class RecipeSerializer(serializers.ModelSerializer):
         recipe.tags.set(tags)
         return recipe
 
-    def update(self, instance, validated_data):
-        instance.image = validated_data.get('image', instance.image)
-        instance.name = validated_data.get('name', instance.name)
-        instance.text = validated_data.get('text', instance.text)
-        instance.cooking_time = validated_data.get(
-            'cooking_time', instance.cooking_time
-        )
-        instance.tags.clear()
-        tags_data = self.initial_data.get('tags')
-        instance.tags.set(tags_data)
-        IngredientInRecipe.objects.filter(recipe=instance).all().delete()
-        self.add_ingredients(
-            ingredients=validated_data.get('ingredients'),
-            recipe=instance
-        )
-        instance.save()
+    def update(self, recipe, validated_data):
+        ingredients = validated_data.pop('ingredients')
+        tags = validated_data.pop('tags')
+        IngredientInRecipe.objects.filter(recipe=recipe).delete()
+        self.add_ingredients(ingredients, recipe)
+        recipe.tags.set(tags)
+        return super().update(recipe, validated_data)
         return instance
 
     def to_representation(self, instance):
