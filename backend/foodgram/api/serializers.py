b26fd08903d4ad7c701d699b8fe9698f334f93a5
Victoriafed <victoriafedorova2018@mail.ru>
2023-07-03 15:49:20 +0300

    v2.1 all

--- backend/foodgram/api/serializers.py
+++ backend/foodgram/api/serializers.py
@@ -250,7 +250,7 @@ class SubscriptionSerializer(UserSerializer):
             'recipes',
             'recipes_count'
         )
-        read_only_fields = ('all')
+        read_only_fields = ('all', )
 
     @staticmethod
     def get_recipes_count(obj):
