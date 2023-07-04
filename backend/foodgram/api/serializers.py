cff773852bd22e450cd90affc6b3cf7c936d32bb
Victoriafed <victoriafedorova2018@mail.ru>
2023-07-04 21:04:09 +0300

    v3 my ver re

--- backend/foodgram/api/serializers.py
+++ backend/foodgram/api/serializers.py
@@ -227,7 +227,7 @@ class ShortRecipeSerializer(serializers.ModelSerializer):
 
 
 class SubscriptionSerializer(UserSerializer):
-    recipes = ShortRecipeSerializer(many=True, read_only=True)
+    recipes = serializers.SerializerMethodField()
     recipes_count = serializers.SerializerMethodField()
 
     class Meta:
@@ -247,6 +247,15 @@ class SubscriptionSerializer(UserSerializer):
     @staticmethod
     def get_recipes_count(obj):
         return obj.recipes.count()
+
+    def get_recipes(self, obj):
+        request = self.context.get('request')
+        limit = request.GET.get('recipes_limit')
+        recipes = obj.recipes.all()
+        if limit:
+            recipes = recipes[:int(limit)]
+        serializer = ShortRecipeSerializer(recipes, many=True, read_only=True)
+        return serializer.data
 
     def validate(self, data):
         author = get_object_or_404(User, self.context.get['id'])
