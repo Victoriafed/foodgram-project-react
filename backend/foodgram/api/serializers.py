3a99fb5a68a4d9ce32d0552d3b1b383c0d106ac1
Victoriafed <victoriafedorova2018@mail.ru>
2023-07-04 18:13:27 +0300

    v3 new subser

--- backend/foodgram/api/serializers.py
+++ backend/foodgram/api/serializers.py
@@ -226,25 +226,23 @@ class ShortRecipeSerializer(serializers.ModelSerializer):
 
 
 class SubscriptionSerializer(UserSerializer):
-    recipes = ShortRecipeSerializer(many=True, read_only=True)
+    recipes = serializers.SerializerMethodField()
     recipes_count = serializers.SerializerMethodField()
 
-    class Meta:
-        model = User
-        fields = (
-            "email",
-            "id",
-            "username",
-            "first_name",
-            "last_name",
-            "is_subscribed",
-            "recipes",
-            "recipes_count",
+    class Meta(UserSerializer.Meta):
+        fields = UserSerializer.Meta.fields + (
+            'recipes_count', 'recipes'
         )
-        read_only_fields = ("all",)
+        read_only_fields = ('email', 'username')
 
-    def get_is_subscribed(*args):
-        return True
+    def get_recipes(self, obj):
+        request = self.context.get('request')
+        limit = request.GET.get('recipes_limit')
+        recipes = obj.recipes.all()
+        if limit:
+            recipes = recipes[:int(limit)]
+        serializer = ShortRecipeSerializer(recipes, many=True, read_only=True)
+        return serializer.data
 
     @staticmethod
     def get_recipes_count(obj):
