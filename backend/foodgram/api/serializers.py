bb13fe2c075b1f35837679bfd1f5d2852a222515
Victoriafed <victoriafedorova2018@mail.ru>
2023-07-04 23:51:34 +0300

    v3 IinR IR

--- backend/foodgram/api/serializers.py
+++ backend/foodgram/api/serializers.py
@@ -197,16 +197,9 @@ class RecipeSerializer(serializers.ModelSerializer):
         return super().update(recipe, validated_data)
 
     def to_representation(self, instance):
-        self.fields.pop('ingredients')
-        self.fields.pop('tags')
-        representation = super().to_representation(instance)
-        representation['ingredients'] = IngredientReadSerializer(
-            IngredientInRecipe.objects.filter(recipe=instance), many=True
-        ).data
-        representation['tags'] = TagSerializer(
-            instance.tags, many=True
-        ).data
-        return representation
+        request = self.context.get('request')
+        context = {'request': request}
+        return RecipeReadSerializer(instance, context=context).data
 
     def validate(self, data):
         ingredients = data['ingredients']
